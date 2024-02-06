4071fd680fc47880bef4920f4bbe3bbd54c6f924
mohamed <mohamedkharashy27@yahoo.com>
2024-02-06 22:15:16 +0000

    1

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
-"""this is Flask app"""
-
+"""Contains a basic flask app displaying 'Welcome to Holberton' on"""
+from os import getenv
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
-class Config(object):
-    """class to Configuration for Babel"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
+app = Flask(__name__, static_url_path='')
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """func to Select and return best language match based"""
-    locale = request.args.get('locale')
-
-    if locale in app.config['LANGUAGES']:
-        return locale
-
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+class Config(object):
+    """class to configuration for babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    app.config.from_object(Config)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def func_index() -> str:
-    """func to Handles / route"""
+    """func to this route renders 0-index.html template"""
     return render_template('4-index.html')
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """func method checks the URL parameter for locale variable"""
+    if request.args.get('locale'):
+        req = request.args.get('locale')
+
+        if req in app.config['LANGUAGES']:
+            return req
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
